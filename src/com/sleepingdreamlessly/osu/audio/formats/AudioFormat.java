19694f4b35be166ec7fd5b6410d49225c7bb0ccf
sleepingdreamlessly <lilly@sleepily.moe>
2018-02-23 15:48:09 +0100

    TODO: make formats iterable for when loading samples

--- src/com/sleepingdreamlessly/osu/audio/formats/AudioFormat.java
+++ src/com/sleepingdreamlessly/osu/audio/formats/AudioFormat.java
@@ -4,15 +4,17 @@ public class AudioFormat
 {
 	public static String ID, NAME;
 	
+	//@TODO: make this iterable for when loading samples in Assets
+	
 	public AudioFormat(String id)
 	{
 		ID = id;
 		
 		NAME =
-			(id == ".wav") ? "Waveform Audio File Format" :
-			(id == ".mp3") ? "MP3" :
-			(id == ".flac") ? "Free Lossless Audio Codec" :
-			(id == ".ogg") ? "Ogg Vorbis" :
+			(id.equals(".wav" )) ? "Waveform Audio File Format" :
+			(id.equals(".mp3" )) ? "MP3" :
+			(id.equals(".flac")) ? "Free Lossless Audio Codec" :
+			(id.equals(".ogg" )) ? "Ogg Vorbis" :
 			"unknown";
 	}
 }
