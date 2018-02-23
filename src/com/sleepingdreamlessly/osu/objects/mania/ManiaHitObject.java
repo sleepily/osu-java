beeed5250d3588811a0ad8f63e7ca5b5eaf73ece
sleepingdreamlessly <lilly@sleepily.moe>
2018-02-23 02:10:24 +0100

    Still not rendering correctly.
    Problems:
    - this.dispose rule (?)
    - this.calculateY() doesn't work anymore

--- src/com/sleepingdreamlessly/osu/objects/mania/ManiaHitObject.java
+++ src/com/sleepingdreamlessly/osu/objects/mania/ManiaHitObject.java
@@ -3,6 +3,7 @@ package com.sleepingdreamlessly.osu.objects.mania;
 import com.sleepingdreamlessly.osu.Game;
 import com.sleepingdreamlessly.osu.objects.HitObject;
 import com.sleepingdreamlessly.osu.rulesets.UI;
+import com.sleepingdreamlessly.osu.utils.Utils;
 import com.sleepingdreamlessly.osu.utils.Vector2;
 
 public class ManiaHitObject extends HitObject
@@ -14,24 +15,25 @@ public class ManiaHitObject extends HitObject
 	public ManiaHitObject(Game game, String id, int x, long time)
 	{
 		super(game, id, x, 0, time);
+		this.pos.x = ((pos.x - 2) * 60 + UI.getJudgementLine().x);
 		this.TYPE = "mania";
 		this.scale = .25f;
 	}
 	
 	public void tick()
 	{
-		if (this.y > UI.getScreenVector().y)
+		if (this.time < this.game.getTime_rel_current_ms() - 200)
 			this.dispose = true;
 		
-		this.calculateY();
-		this.calculateAlpha();
+		// this.calculateY();
+		// this.calculateAlpha();
 	}
 	
 	public void render(UI ui)
 	{
 		this.sprite.drawCenteredWithScale(
 			this.game,
-			(int)((pos.x - 2) * 60 + ui.getJudgementLine().x),
+			(int)(pos.x),
 			(int)(pos.y),
 			this.scale,
 			this.alpha
@@ -40,7 +42,14 @@ public class ManiaHitObject extends HitObject
 	
 	private void calculateY()
 	{
+		if (game.getTime_rel_current_ms() < 10)
+		{
+			this.pos.y = 0;
+			return;
+		}
+		
 		this.pos.y = (UI.getJudgementLine().y) - (this.time - game.getTime_rel_current_ms()) / (float)(20f / UI.getScrollSpeed());
+		// this.pos.y = (int) Utils.map((float)game.getTime_rel_current_ms(), (float)(this.time - (20f / UI.getScrollSpeed())), this.time, -50, UI.getJudgementLine().y);
 	}
 	
 	protected void calculateAlpha()
