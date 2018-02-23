40f3d7b2a53b329e0ec1bb6f32f9dd990184b68d
sleepingdreamlessly <lilly@sleepily.moe>
2018-02-23 01:21:02 +0100

    Add SDVX/Mania specific keys

--- src/com/sleepingdreamlessly/osu/input/KeyManager.java
+++ src/com/sleepingdreamlessly/osu/input/KeyManager.java
@@ -6,33 +6,46 @@ import java.awt.event.KeyListener;
 public class KeyManager implements KeyListener {
 	
 	private boolean[] keys;
-	public boolean std_left, std_right, up, down, left, right;
+	public boolean std_left, std_right;
+	public boolean mania_0, mania_1, mania_2, mania_3;
+	public boolean mania_vol_l_counter, mania_vol_l_clock, mania_vol_r_counter, mania_vol_r_clock, mania_fx_l, mania_fx_r;
 	
-	public KeyManager(){
+	public KeyManager()
+	{
 		keys = new boolean[256];
 	}
 	
 	public void tick(){
-		std_left = keys[KeyEvent.VK_Z];
-		std_right = keys[KeyEvent.VK_X];
-		up = keys[KeyEvent.VK_UP];
-		down = keys[KeyEvent.VK_DOWN];
-		left = keys[KeyEvent.VK_LEFT];
-		right = keys[KeyEvent.VK_RIGHT];
+		std_left = keys[KeyEvent.VK_X];
+		std_right = keys[KeyEvent.VK_C];
+		mania_0 = keys[KeyEvent.VK_D];
+		mania_1 = keys[KeyEvent.VK_F];
+		mania_2 = keys[KeyEvent.VK_J];
+		mania_3 = keys[KeyEvent.VK_K];
+		mania_vol_l_counter = keys[KeyEvent.VK_Q];
+		mania_vol_l_clock = keys[KeyEvent.VK_W];
+		mania_vol_r_counter = keys[KeyEvent.VK_O];
+		mania_vol_r_clock = keys[KeyEvent.VK_P];
+		mania_fx_l = keys[KeyEvent.VK_V];
+		mania_fx_r = keys[KeyEvent.VK_N];
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e)
+	{
 		keys[e.getKeyCode()] = true;
+		System.out.println("KEY pressed: " + e.getKeyChar());
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent e)
+	{
 		keys[e.getKeyCode()] = false;
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e)
+	{
 		
 	}
 	
