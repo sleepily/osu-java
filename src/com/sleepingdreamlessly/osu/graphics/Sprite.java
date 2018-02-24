1f9590dda807e69e9ed500e2362202798753cede
sleepingdreamlessly <lilly@sleepily.moe>
2018-02-24 13:43:43 +0100

    Fix rotation

--- src/com/sleepingdreamlessly/osu/graphics/Sprite.java
+++ src/com/sleepingdreamlessly/osu/graphics/Sprite.java
@@ -3,6 +3,7 @@ package com.sleepingdreamlessly.osu.graphics;
 import com.sleepingdreamlessly.osu.Game;
 import com.sleepingdreamlessly.osu.assets.Assets;
 import com.sleepingdreamlessly.osu.utils.ImageLoader;
+import com.sleepingdreamlessly.osu.utils.Utils;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -30,18 +31,6 @@ public class Sprite
 		this.size = this.i.getWidth(null);
 	}
 	
-	/*
-		https://stackoverflow.com/questions/8639567/java-rotating-images#8639615
-			double rotationRequired = Math.toRadians (45);
-			double locationX = image.getWidth() / 2;
-			double locationY = image.getHeight() / 2;
-			AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
-			AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-			
-			// Drawing the rotated image at the required drawing locations
-			g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
-	 */
-	
 	//@TODO: rework drawing to use an anchor instead of seperate methods
 	
 	public void drawCenteredWithSize(Game game, int x, int y, double size, float opacity)
@@ -62,22 +51,9 @@ public class Sprite
 		draw(game, x, y, image_scaled, opacity);
 	}
 	
-	private void drawRotatedRAD(Game game, int x, int y, Image image, float opacity, double rotationRAD)
-	{
-		double centerX = image.getWidth(null) / 2;
-		double centerY = image.getHeight(null) / 2;
-		AffineTransform tx = AffineTransform.getRotateInstance(rotationRAD, centerX, centerY);
-		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-		
-		//@TODO: finish this and op.filter()? needs to be BufferedImage?
-		draw(game, x, y, image,  opacity);
-	}
-	
-	private void drawRotatedDEG(Game game, int x, int y, Image image, float opacity, double rotationDEG)
-	{
-		double rotationInRAD = Math.toRadians(rotationDEG);
-		drawRotatedRAD(game, x, y, image, opacity, rotationInRAD);
-	}
+	/*
+		draw with rotation referenced from https://stackoverflow.com/questions/8639567/java-rotating-images#8639615
+	 */
 	
 	private void draw(Game game, int x, int y, Image image, float opacity)
 	{
