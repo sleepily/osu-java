1d454f81a55a0a0b4d18c826dc0a6856151c2730
sleepingdreamlessly <lilly@sleepily.moe>
2018-02-25 23:22:38 +0100

    TODO fix InputOverlay position

---
+++ src/com/sleepingdreamlessly/osu/objects/InputOverlay.java
@@ -0,0 +1,48 @@
+package com.sleepingdreamlessly.osu.objects;
+
+import com.sleepingdreamlessly.osu.Handler;
+import com.sleepingdreamlessly.osu.rulesets.UI;
+import com.sleepingdreamlessly.osu.utils.Utils;
+
+public class InputOverlay extends GameObject
+{
+	private InputOverlayKey[] keys = new InputOverlayKey[4];
+	public int width, height;
+	
+	public InputOverlay(Handler handler)
+	{
+		super(handler.getGame(), "inputoverlay-background");
+		keys[0] = new InputOverlayKey(handler, "std_left", 0);
+		keys[1] = new InputOverlayKey(handler, "std_right", 1);
+		keys[2] = new InputOverlayKey(handler, "std_mouse_left", 2);
+		keys[3] = new InputOverlayKey(handler, "std_mouse_right", 3);
+		this.sprite.i = Utils.rotateImage(this.sprite.i, 90);
+		
+		//@TODO: calculate correct position?
+		x = UI.getScreenVector().x;
+		y = UI.getScreenVector().y / 2;
+		
+		width = sprite.i.getWidth(null);
+		height = sprite.i.getHeight(null);
+	}
+	
+	public void tick()
+	{
+		for (InputOverlayKey iok : keys)
+			iok.tick();
+	}
+	
+	public void render(UI ui)
+	{
+		this.sprite.drawCenteredWithScale(
+			this.game,
+			(int)(x),
+			(int)(y),
+			1f,
+			1f
+		);
+		
+		for (InputOverlayKey iok : keys)
+			iok.render();
+	}
+}
