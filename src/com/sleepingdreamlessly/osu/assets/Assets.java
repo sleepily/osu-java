c4ee41a7b538c75f11951a89449dbeb5e0b90813
sleepingdreamlessly <lilly@sleepily.moe>
2018-03-02 11:02:53 +0100

    Add OsuJudgement sprites to Assets

--- src/com/sleepingdreamlessly/osu/assets/Assets.java
+++ src/com/sleepingdreamlessly/osu/assets/Assets.java
@@ -20,6 +20,7 @@ public class Assets
 	// osu
 	public static Sprite inputoverlay_background, inputoverlay_key;
 	public static Sprite hitcircle, hitcircleoverlay, approachcircle, cursor, cursortrail;
+	public static Sprite std_hit0, std_hit50, std_hit50k, std_hit100, std_hit100k, std_hit300, std_hit300g, std_hit300k;
 	
 	// mania
 	public static Sprite note;
@@ -55,6 +56,14 @@ public class Assets
 		hitcircle 				= new Sprite(game, "hitcircle");        _sprites.add(hitcircle);
 		approachcircle		= new Sprite(game, "approachcircle");   _sprites.add(approachcircle);
 		hitcircleoverlay 	= new Sprite(game, "hitcircleoverlay"); _sprites.add(hitcircleoverlay);
+		std_hit0          = new Sprite(game, "hit0");             _sprites.add(std_hit0);
+		std_hit50         = new Sprite(game, "hit50");            _sprites.add(std_hit50);
+		std_hit50k        = new Sprite(game, "hit50k");           _sprites.add(std_hit50k);
+		std_hit100        = new Sprite(game, "hit100");           _sprites.add(std_hit100);
+		std_hit100k       = new Sprite(game, "hit100k");          _sprites.add(std_hit100k);
+		std_hit300        = new Sprite(game, "hit300");           _sprites.add(std_hit300);
+		std_hit300k       = new Sprite(game, "hit300k");          _sprites.add(std_hit300k);
+		std_hit300g       = new Sprite(game, "hit300g");          _sprites.add(std_hit300g);
 		
 		// mania
 		note              = new Sprite(game, "note");             _sprites.add(note);
